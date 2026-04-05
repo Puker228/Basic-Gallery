8edf2644c591a6d50b8fc90c6f408f99f2d8a896
puker228 <pochta@pochta.com>
2026-04-06 02:56:04 +0300

    change version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 4
-        versionName = "1.0.5"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
