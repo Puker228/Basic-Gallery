5c70f68b9e18d2ee671b6b4080588260c351257e
puker228 <pochta@pochta.com>
2026-04-05 23:06:33 +0300

    update ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.basicgallery"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.4"
+        versionCode = 4
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
